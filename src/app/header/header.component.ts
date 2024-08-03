b468ee750fc832a0708aebfa4a8de629f1e2c6eb
nika todua <alchimikos@gmail.com>
2024-08-03 11:43:25 +0400

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,7 +10,7 @@ export class HeaderComponent {
   countevent: number = 0
   toggleclick() {
     this.countevent++
-    if(this.countevent % 2 === 1){this.animclass = "active"} else{this.animclass = ""}
+    if(this.countevent % 2 === 1){this.animclass = "active";document.body.classList.add("no-scroll")} else{this.animclass = "";document.body.classList.remove("no-scroll")}
   }
 
 }
